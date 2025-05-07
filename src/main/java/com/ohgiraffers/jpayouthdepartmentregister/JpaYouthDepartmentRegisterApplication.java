37ba5896d9359b5f8b2277602218e92ffa43bc3d
Cori1304-Seong <andyjm7739@gmail.com>
2025-05-07 14:09:57 +0900

    boilerplate

---
+++ src/main/java/com/ohgiraffers/jpayouthdepartmentregister/JpaYouthDepartmentRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.jpayouthdepartmentregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaYouthDepartmentRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaYouthDepartmentRegisterApplication.class, args);
+    }
+
+}
