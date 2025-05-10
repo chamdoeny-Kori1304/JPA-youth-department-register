0418851d840bd219ca9fee5c022ab8a04fd131fd
Cori1304-Seong <andyjm7739@gmail.com>
2025-05-10 22:36:23 +0900

    feat: Add findBySmallGroupName()

--- src/main/java/com/kori1304/jpayouthdepartmentregister/member/infrastructure/MembersJpaAdapter.java
+++ src/main/java/com/kori1304/jpayouthdepartmentregister/member/infrastructure/MembersJpaAdapter.java
@@ -1,6 +1,8 @@
 package com.kori1304.jpayouthdepartmentregister.member.infrastructure;
 
+import com.kori1304.jpayouthdepartmentregister.member.domain.Member;
 import com.kori1304.jpayouthdepartmentregister.member.domain.Members;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -12,12 +14,39 @@ class MembersJpaAdapter implements Members {
   private final MemberRepository memberRepository;
 
   @Override
-  public MemberEntity save(MemberEntity member) {
-    return memberRepository.save(member);
+  public Member save(Member member) {
+    MemberEntity entity = MemberEntity.fromDomain(member);
+
+    return memberRepository.save(entity).toDomain();
   }
 
   @Override
-  public List<MemberEntity> findAll() {
-    return memberRepository.findAll();
+  public List<Member> findAll() {
+    List<Member> result = new ArrayList<>();
+    List<MemberEntity> members = memberRepository.findAll();
+
+    for (MemberEntity member : members) {
+      result.add(member.toDomain());
+    }
+
+    return result;
+  }
+
+  @Override
+  public List<Member> findBySmallGroupName(String name) {
+    List<Member> result = new ArrayList<>();
+    List<MemberEntity> members = memberRepository.findAllBySmallGroupEntity_Name(name);
+
+    for (MemberEntity member : members) {
+      result.add(member.toDomain());
+    }
+
+    return result;
+  }
+
+  @Override
+  public Member findByName(String name) {
+
+    return memberRepository.findByName(name).toDomain();
   }
 }
