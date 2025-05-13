657a0da6b1001132af0033611e495021c577d1ef
Cori1304-Seong <andyjm7739@gmail.com>
2025-05-13 17:46:09 +0900

    feat:Add getMembersByNameContaining

--- src/main/java/com/kori1304/jpayouthdepartmentregister/member/application/MemberService.java
+++ src/main/java/com/kori1304/jpayouthdepartmentregister/member/application/MemberService.java
@@ -1,13 +1,18 @@
 package com.kori1304.jpayouthdepartmentregister.member.application;
 
 
+import com.kori1304.jpayouthdepartmentregister.member.api.dto.MemberPostDTO;
 import com.kori1304.jpayouthdepartmentregister.member.domain.Member;
 import com.kori1304.jpayouthdepartmentregister.member.domain.Members;
+import com.kori1304.jpayouthdepartmentregister.member.small_group.SmallGroup;
+import jakarta.transaction.Transactional;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,9 +24,28 @@ import java.util.List;
 public class MemberService {
 
   private final Members members; // 도메인 리포지토리 인터페이스
+  private final ModelMapper modelMapper;
 
+
+  @Transactional
   public boolean add(Member member) {
     try {
+      members.save(member);
+
+      return true;
+    } catch (Exception e) {
+      log.error("Failed to save member: " + e.getMessage());
+      return false;
+    }
+  }
+
+  @Transactional
+  public boolean add(MemberPostDTO dto, SmallGroup smallGroup) {
+    try {
+
+      Member member = dto.toDomain();
+      member.setSmallGroup(smallGroup);
+
       members.save(member);
 
       return true;
@@ -40,7 +64,7 @@ public class MemberService {
     List<Member> allMembers = getAllMembers();
 
     for (Member member : allMembers) {
-      result.computeIfAbsent(member.getSmallGroup().getName(), k -> new ArrayList<>())
+      result.computeIfAbsent(member.getSmallGroup().getId().toString(), k -> new ArrayList<>())
           .add(member.getName());
     }
 
@@ -55,10 +79,15 @@ public class MemberService {
     return members.findByName(name);
   }
 
-  public List<Member> getMembersBySmallGroup(String groupName) {
-    return members.findBySmallGroupName(groupName);
+  public List<Member> getMembersBySmallGroup(Long id) {
+    return members.findBySmallGroupId(id);
   }
 
+  public List<Member> getMembersByNameContaining(String name) {
+    return members.findByNameContaining(name);
+  }
+
+  @Transactional
   public boolean setMemberInfo(Member member) {
     try {
       members.updateInfo(member);
