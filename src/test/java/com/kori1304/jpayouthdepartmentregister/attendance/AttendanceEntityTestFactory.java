ef8079f75f9a39d9ea5d7717d4e04e1b29064b9b
Cori1304-Seong <andyjm7739@gmail.com>
2025-05-16 16:28:01 +0900

    test: Add Attendance factory code

---
+++ src/test/java/com/kori1304/jpayouthdepartmentregister/attendance/AttendanceEntityTestFactory.java
@@ -0,0 +1,54 @@
+package com.kori1304.jpayouthdepartmentregister.attendance;
+
+import com.kori1304.jpayouthdepartmentregister.attendance.infrastructure.AttendanceEntity;
+import com.kori1304.jpayouthdepartmentregister.member.domain.Gender;
+import com.kori1304.jpayouthdepartmentregister.member.infrastructure.MemberEntity;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AttendanceEntityTestFactory {
+
+  private static final Object[][] SAMPLE_DATA = {
+      {1L, LocalDate.now(), true, 1L},
+      {2L, LocalDate.now().minusDays(1), true, 1L},
+      {3L, LocalDate.now().minusDays(2), false, 2L},
+      {4L, LocalDate.now().minusDays(3), true, 2L},
+      {5L, LocalDate.now().minusDays(4), false, 3L},
+      {6L, LocalDate.now().minusDays(5), true, 3L},
+      {7L, LocalDate.now().minusDays(6), false, 4L},
+      {8L, LocalDate.now().minusDays(7), true, 4L},
+      {9L, LocalDate.now().minusDays(8), false, 5L},
+      {10L, LocalDate.now().minusDays(9), true, 5L}
+  };
+
+  public static List<AttendanceEntity> createSampleEntities() {
+    List<AttendanceEntity> entities = new ArrayList<>();
+
+    for (Object[] row : SAMPLE_DATA) {
+      entities.add(AttendanceEntity.builder()
+          .date((LocalDate) row[1])
+          .isAttendance((Boolean) row[2])
+          .memberId((Long) row[3])
+          .build()
+      );
+    }
+
+    return entities;
+  }
+
+  public static AttendanceEntity getSampleMemberEntity(int number) {
+    if (number > 9 || number < 0) {
+      return null;
+    }
+
+    Object[] row = SAMPLE_DATA[number];
+
+    return AttendanceEntity.builder()
+        .date((LocalDate) row[1])
+        .isAttendance((Boolean) row[2])
+        .memberId((Long) row[3])
+        .build();
+  }
+}
+
