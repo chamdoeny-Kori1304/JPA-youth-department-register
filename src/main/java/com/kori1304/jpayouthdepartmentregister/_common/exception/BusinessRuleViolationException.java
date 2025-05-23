713fb6410a40fd501deb871996e89a28e759962c
Cori1304-Seong <andyjm7739@gmail.com>
2025-05-23 17:59:09 +0900

    feat: Add BusinessRuleViolationException

---
+++ src/main/java/com/kori1304/jpayouthdepartmentregister/_common/exception/BusinessRuleViolationException.java
@@ -0,0 +1,8 @@
+package com.kori1304.jpayouthdepartmentregister._common.exception;
+
+public class BusinessRuleViolationException extends RuntimeException {
+
+  public BusinessRuleViolationException(String message) {
+    super(message);
+  }
+}
