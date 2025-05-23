37fd7a1d2c71210a9dec9cb5a744b17a86eef5ee
Cori1304-Seong <andyjm7739@gmail.com>
2025-05-23 17:59:58 +0900

    feat: Add TokenProviderImpl

--- src/main/java/com/kori1304/jpayouthdepartmentregister/_common/jwt/TokenProvider.java
+++ src/main/java/com/kori1304/jpayouthdepartmentregister/_common/jwt/TokenProvider.java
@@ -1,12 +1,12 @@
 package com.kori1304.jpayouthdepartmentregister._common.jwt;
 
 import com.kori1304.jpayouthdepartmentregister._common.jwt.dto.TokenDTO;
-import com.kori1304.jpayouthdepartmentregister.user.infrastructure.UserEntity;
+import com.kori1304.jpayouthdepartmentregister.user.domain.model.User;
 import org.springframework.security.core.Authentication;
 
 public interface TokenProvider {
 
-  TokenDTO generateTokenDTO(UserEntity user);
+  TokenDTO generateTokenDTO(User user);
 
   String getUserId(String token);
 
