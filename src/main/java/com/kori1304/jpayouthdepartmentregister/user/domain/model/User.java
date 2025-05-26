c0bd318396249469fa49ef186d5f7177cc69385d
Cori1304-Seong <andyjm7739@gmail.com>
2025-05-26 17:39:01 +0900

    feat: Enable Spring Security integration for User

--- src/main/java/com/kori1304/jpayouthdepartmentregister/user/domain/model/User.java
+++ src/main/java/com/kori1304/jpayouthdepartmentregister/user/domain/model/User.java
@@ -1,24 +1,31 @@
 package com.kori1304.jpayouthdepartmentregister.user.domain.model;
 
+import java.util.Collection;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
-public class User {
-
-
-
+public class User implements UserDetails {
   private Long id;
   private String name;
   private String email;
   private String password;
   private List<Role> userRoles;
+  private Collection<GrantedAuthority> authorities;
+
+  @Override
+  public String getUsername() {
+    return name;
+  }
+
 }
